27a234adb1e3b7e1c783c199c80faabfed39e97c
abhaykrishnamu <abhaykrishna88mu@gmail.com>
2026-03-16 19:09:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ except ImportError:
 import streamlit as st
 import google.generativeai as genai
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
 import os
