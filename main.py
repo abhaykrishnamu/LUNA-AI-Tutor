8e4308ec286f388d0f3bc169b781d16c7f4187fc
abhaykrishnamu <abhaykrishna88mu@gmail.com>
2026-04-04 10:48:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ except:
 # --- 3. LIBRARIES ---
 import google.generativeai as genai
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
 
